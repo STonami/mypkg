158b58ff6687431aa20defb0ea8da8fc3b937717
Tonami S <dounanguan@gmail.com>
2025-02-11 17:31:26 +0900

    Add a command

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import os
 from glob import glob
 
 package_name = 'mypkg'
@@ -7,12 +8,11 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    py_modules=[],
     data_files=[
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py'))
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
