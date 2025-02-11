3557e1ccc53dbfa14f873bfad8f15d3068ccf9fd
Tonami S <dounanguan@gmail.com>
2025-02-11 17:43:07 +0900

    Add a command

--- test/test.bash
+++ test/test.bash
@@ -9,10 +9,15 @@ cd $dir/ros2_ws
 
 colcon build
 
+# ROS 2 の環境を適用
 source $dir/ros2_ws/install/setup.bash
+export PATH=$dir/ros2_ws/install/bin:$PATH
+exec bash
 
+# ノードを起動
 timeout 60 ros2 launch mypkg talk_listen.launch.py > /tmp/powerwatch.log
 
+# ログを解析
 if grep -q 'battery_status_listener' /tmp/powerwatch.log; then
     echo "Test passed: Listener found in the log."
     exit 0
@@ -20,3 +25,4 @@ else
     echo "Test failed: Listener not found in the log."
     exit 1
 fi
+
