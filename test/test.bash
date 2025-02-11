296beac5c6b63dcb5909f02513f7fc1a1abc90ba
Tonami S <dounanguan@gmail.com>
2025-02-11 16:39:02 +0900

    Add a command

--- test/test.bash
+++ test/test.bash
@@ -8,23 +8,14 @@ dir=~
 
 # ROS 2 ワークスペースに移動してビルド
 cd $dir/ros2_ws
+
 colcon build
+
 source $dir/.bashrc
 source $dir/ros2_ws/install/setup.bash
 
 # ノードをバックグラウンドで実行
-ros2 run mypkg battery_status_publisher > /tmp/battery_status.log 2>&1 &
-NODE_PID=$!
-
-# ノードがデータをパブリッシュするのを待つ
-sleep 5
-
-# トピックからメッセージを購読
-timeout 100 ros2 topic echo /battery/percents > /tmp/battery_status_test.log
-
-# ノードを停止
-kill $NODE_PID
-wait $NODE_PID
+timeout 60 ros2 launch mypkg talk_listen.launch.py > /tmp/battery_status.log 2>&1 &
 
 # ログの内容を検証
 if grep -E 'Battery:' /tmp/battery_status_test.log; then
